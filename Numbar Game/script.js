cbf08796b72ecbbe132d33c6ff692f163033bfe1
Dhruvan Patel <130367301+dhruvan12@users.noreply.github.com>
2024-05-28 09:44:48 +0530

    final commit

--- Numbar Game/script.js
+++ Numbar Game/script.js
@@ -1,105 +1,43 @@
-let randomNum = parseInt(Math.random() * 100 + 1)
-// console.log(randomNum);
+function navAnimation () {
 
-const userInput = document.querySelector('#guessField')
-const submit = document.querySelector('#subt')
-const lowOrHi = document.querySelector('.lowOrHi')
-const remaining = document.querySelector('.lastResult')
-const guessSlot = document.querySelector('.guesses')
-const startOver = document.querySelector('.resultParas');
+    let nav = document.querySelector("nav")
 
-const p = document.createElement('p')
-
-let prevGuess = [];
-let numGuess = 1;
-
-let playGame = true;
-
-if (playGame) {
-    submit.addEventListener('click', function (e) {
-        e.preventDefault();
-        const guess = parseInt(userInput.value)
-        validateGuess(guess);
-
-    });
+    nav.addEventListener("mouseenter", function () {
+        let tl = gsap.timeline()
+        tl.to("#nav-bottom", {
+            height: "20vh"
+        })
+        tl.to(".nav-part2 h5", {
+            display: "block"
+        })
+        tl.to(".nav-part2 h5 span", {
+            y: 0,
+            stagger: {
+                amount: 0.4
+            }
+        })
+    })
+    
+    nav.addEventListener("mouseleave", function () {
+        let tl = gsap.timeline()
+    
+        // tl.to(".nav-part2 h5 span", {
+        //     y: 15,
+        //     stagger: {
+        //         amount: 0
+        //     }
+        // })
+    
+        tl.to(".nav-part2  h5", {
+            display: "none",
+            duration: 0.2
+        })
+        tl.to("#nav-bottom", {
+            height: 0,
+            duration: 0.1
+        })
+    
+    })  
 }
 
-
-function validateGuess(guess) {
-    if (isNaN(guess)) {
-        alert("Please enter the valid number !!");
-    }
-    else if (guess < 1) {
-        alert("Please enter the grather then 0 number !!");
-    }
-    else if (guess > 100) {
-        alert("Please enter the less then 100 number !!");
-    }
-    else {
-        prevGuess.push(guess);
-        if (numGuess === 10) {
-            displayGuess(guess);
-            dispalyMessage(`Game was over and Random number was ${randomNum}`);
-            endGame();
-        }
-        else {
-            displayGuess(guess);
-            checkguess(guess);
-        }
-    }
-}
-
-
-
-function checkguess(guess) {
-    if (randomNum === guess) {
-        dispalyMessage(`You gessed it right`)
-    }
-    else if (guess > randomNum) {
-        dispalyMessage(`The guess is Too High`)
-
-    }
-    else if (guess < randomNum) {
-        dispalyMessage(`The guess is Too Low`)
-
-    }
-}
-
-function displayGuess(guess) {
-
-    userInput.value = ''
-    guessSlot.innerHTML += `${guess}  `
-    numGuess++ ;
-    remaining.innerHTML = `${11-numGuess}`
-
-}
-function dispalyMessage(message) {
-lowOrHi.innerHTML = ` <h2>${message}</h2>`
-}
-function endGame() {
-userInput.value = ''
-userInput.setAttribute('disabled' , '')
-p.classList.add('button')
-p.innerHTML = `<h2 id="newGame">Start new Game</h2>`;
-startOver.appendChild(p);
-playGame = false;
-newGame();
-}
-
-function newGame() {
-    const newGameButton = document.querySelector('#newGame');
-    newGameButton.addEventListener( 'click', function (e) {
-        randomNumber = parseInt(Math.random() * 100 + 1);
-        prevGuess = []
-        numGuess = 1;
-        guessSlot.innerHTML = '';
-        remaining.innerHTML = `${11 - numGuess} `;
-        userInput.removeAttribute('disabled');
-        lowOrHi.innerHTML=''
-        startOver.removeChild(p);
-        dispalyMessage.innerHTML = ''
-        playGame = true;
-
-    });
-
-}
+navAnimation()
